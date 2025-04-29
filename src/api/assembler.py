wip: wrong result

src/api/assembler.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 import sympy as sp
+import numpy as np
 
 from api.ast.fns import FFnType
 from api.mesh.mesh import Mesh
@@ -7,10 +8,29 @@ from api.mesh.mesh import Mesh
 from api.integrator import integrate
 
 def assemble(mesh : Mesh, expr : sp.Expr, functions : List[Tuple[sp.Function, FFnType]]):
+    nbnode = len(mesh.nodes)
+
+    nbtest = len([fn for fn in functions if fn.type == FFnType.TEST])
+    nbunknown = len([fn for fn in functions if fn.type == FFnType.UNKNOWN])
+
+    K = np.zeros((nbunknown*nbnode, nbtest*nbnode), dtype=float)
+    f = np.zeros(nbunknown * nbnode, dtype=float)
+
     for elem, conn in mesh.elems.items():
-        for i in range(conn.shape[0]):
-            nodes = mesh.nodes[conn[i]]
+        nbnode = conn.shape[1]
+        for cur in range(conn.shape[0]):
+            node_ids = conn[cur]
+            nodes = mesh.nodes[node_ids]
             local_K, local_F = integrate(nodes, elem, expr, functions)
-            print("K IS \n", local_K)
-            print("F IS \n", local_F)
-         
+            assert local_F.shape[0] == local_K.shape[0]
+            assert local_K.shape[0] == nbunknown * nbnode
+
+            # Assume DOFs are same as node IDs
+            for i in range(nbnode):
+                row = node_ids[i]
+                for u in range(nbunknown):
+                    f[row * nbunknown + u] += local_F[i]
+                    for j in range(nbnode):
+                        col = node_ids[j]
+                        K[row * nbunknown + u, col] += local_K[i, j]
+    return K, f

src/api/integrator.py
@@ -22,7 +22,7 @@ def integrate(nodes : NDArray[floating], elem : FElement, expr : Expr, functions
     nb_dof = nb_ref_pts * len(unknowns)
     nb_test = nb_ref_pts * len([ fn for fn in functions if fn.type == FFnType.TEST ])
     K = np.zeros((nb_dof, nb_test), dtype=float)
-    f = np.zeros(nb_test, dtype=float)
+    f = np.zeros(nb_dof, dtype=float)
     i = 0
     for i in range(len(elem.ips)):
 
@@ -72,5 +72,5 @@ def integrate(nodes : NDArray[floating], elem : FElement, expr : Expr, functions
         else:
             K, f = evaluate(expr, K, f)
         K *= linalg.det(J)
-    return J * K, f
+    return J * K, f.flatten()
 

src/examples/basic_1D.py
@@ -1,4 +1,5 @@
 from sympy import *
+import numpy as np
 
 from api.ast.fns import FFnType, create_test_fn, create_unknown_fn
 from api.fem import create_system
@@ -9,11 +10,24 @@ from api.mesh.utils import discretize_1d
 u = create_unknown_fn()
 v = create_test_fn()
 
-f = 2
+f = 1
 
 expr = diff(u) * diff(v) - f * v
 
-mesh = discretize_1d(0, 1, 2)
+mesh = discretize_1d(0, 1, 8)
 
-create_system(mesh, expr, [u, v])
-# K, f = create_system(mesh, ast, [u, v])
+K, f = create_system(mesh, expr, [u, v])
+
+# Set DOF 0 and DOF -1 to 0
+dofs_to_constrain = [0, -1]
+for dof in dofs_to_constrain:
+    # Set the row and column to identity
+    K[dof, :] = 0
+    K[:, dof] = 0
+    K[dof, dof] = 1
+    
+    # Set the corresponding force to 0
+    f[dof] = 0
+
+u = np.linalg.solve(K, f)
+print(u)